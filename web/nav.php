769d624c36395dd3e22d784407d77042f065df5f
PGabo <55789247+PGabo@users.noreply.github.com>
2020-04-10 11:59:28 +0200

    Nav.php létrehozása
    navigáció html része elkészült

---
+++ web/nav.php
@@ -0,0 +1,22 @@
+<?php
+    echo
+    "<nav class='navbar navbar-expand-lg navbar-light bg-light'> 
+      <a href='index.php' class='navbar-brand'><img class='logo' src='logo.png' alt='logo' title='logo' /></a>
+      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'></span>
+      </button>
+      <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul class='navbar-nav mr-auto'>
+          <li class='nav-item'>
+            <a class='nav-link' href='index.php'>Kezdőlap</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='$regphp'>$reg</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='$log'>$name</a>
+          </li>
+        </ul>
+      </div>
+</nav>";
+    ?>
