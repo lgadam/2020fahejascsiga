ba7e6c32d49dd393ff70bd911bf3561352b6de4a
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-04-15 10:21:43 +0200

    Update login.php
    Login kiegészítése tartalommal

--- web/login.php
+++ web/login.php
@@ -8,6 +8,24 @@
     <div class="col-sm-6">
         <div class="row">
             <div class="col-xs-12 container border">
+                <h3>Bejelentkezés </h3>
+                <hr>
+                <form class="modifyform" name="insert" action="" method="post">
+                    <h3><?php echo $_SESSION['action1']; ?></h3>
+                    <div>
+                        <label>Email</label>
+                        <input type="email" name="email" id="email" value="" class="form-control" required/>
+                    </div>
+                    <div>
+                        <label>Jelszó</label>
+                        <input type="password" name="password" id="password" value="" class="form-control" required/>
+                    </div>
+                    <div><input type="submit" name="login" value="Bejelentkezés" class="submitbtn btn btn-primary"/>
+                    </div>
+                    <div class="pswremcon">
+                        Elfelejtetted a jelszavad? Akkor kattints <a href="pswreset.php" class="btn btn-success">ide.</a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
