904e52f8279052921077bf63823ef4c5316344f1
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-04-15 10:10:17 +0200

    Update login.php
    Bejelentkezéshez a törzs megadása.

--- web/login.php
+++ web/login.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="hu">
+<?php require('./html/head.html');?>
+<body class="login-body">
+<?php require("nav.php"); ?>
+<main>
+<div class="container-fluid">
+    <div class="col-sm-6">
+        <div class="row">
+            <div class="col-xs-12 container border">
+            </div>
+        </div>
+    </div>
+</div>
+</main>
+<?php require('./html/footer.html'); ?>
+</body>
+</html>
