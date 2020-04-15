686aa71af0ec55527fb206384a1bdfa05cffd7b4
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-04-15 10:53:03 +0200

    Update login.php
    Login funkcióinak a kijavítása

--- web/login.php
+++ web/login.php
@@ -13,6 +13,17 @@ if (isset($_POST['login']) && $_POST['login']) {
     if ($user === false)
         loginErrorMessage();
 
+    if (!$acc->CheckPassword($user['id'], $_POST['password']))
+        loginErrorMessage();
+    $_SESSION['login'] = $email;
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['name'] = $user['name'];
+    $_SESSION['admin'] = $user['admin'];
+    if ($user['status'] == 0) {
+        $_SESSION['action1'] = "Erősítse meg e-mail címét! A link az e-mailjei között található.";
+    } else {
+        header("Location:index.php");
+    }
 }
 ?>
     
