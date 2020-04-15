b9ceb43354105e3b126d9ca6a7f90939e8d5c2dc
PGabo <55789247+PGabo@users.noreply.github.com>
2020-04-15 10:41:55 +0200

    Register php létrehozása és a váz megírása

---
+++ web/register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<?php require('./html/head.html') ?>
+<body>
+<?php include_once("nav.php"); ?>
+<main class="container-fluid">
+
+</main>
+<?php require('./html/footer.html'); ?>
+</body>
+</html>
