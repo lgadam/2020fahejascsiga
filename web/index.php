e10e349adf4c3746468d649dfe8d5f00ae0d371f
fepu08 <besenyeiferenc@protonmail.com>
2020-04-15 12:08:38 +0200

    index.php létrehozása, header nav footer required

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+/*$db = new Database();
+$connection = $db->DB_Connect();*/
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<?php require('./html/head.html'); ?>
+<body>
+<?php require("nav.php"); ?>
+<main>
+    <div class="_container_">
+        
+    </div>
+</main>
+<?php require('./html/footer.html'); ?>
+</body>
+</html>
