1198484919d2df6081b770016e68171b7bbbe35b
fepu08 <besenyeiferenc@protonmail.com>
2020-04-15 12:12:56 +0200

    config.php elkészítése, megírása

---
+++ web/config/config.php
@@ -0,0 +1,23 @@
+<?php
+class Database{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "root";
+    private $database = "hasznaltautok";
+    private $dbConnection;
+    public function DB_Connect(){
+        try{
+            $this->dbConnection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+            $this->dbConnection->exec('SET NAMES utf8');
+        }
+        catch(PDOException $e)
+        {
+            exit("Connection failed: " . $e->getMessage());
+        }
+        return $this->dbConnection;
+    }
+    public function Disconnect()
+    {
+        $this->dbConnection = null;
+    }
+}
