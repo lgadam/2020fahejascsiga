47ca6e6c082a4ca5eccf9e7467930f58a2ab71fd
PGabo <55789247+PGabo@users.noreply.github.com>
2020-04-19 13:57:01 +0200

    user.php UpdatePassword funkció elkészült

--- web/config/User.php
+++ web/config/User.php
@@ -103,7 +103,16 @@ class Account
         return $sql->execute($data);
     }
 
-    public function UpdatePassword(){
+    public function UpdatePassword(){		
+        $options = [
+            'cost' => 15
+        ];
+        $passwordhash = password_hash($password, PASSWORD_BCRYPT, $options);
+        $sql = $this->con->prepare("UPDATE userregistration SET password = :passwordhash  WHERE email = :email");
+        $sql->bindParam(':passwordhash', $passwordhash, PDO::PARAM_STR);
+        $sql->bindParam(':email', $email, PDO::PARAM_STR);
+        $sql->execute();
+        return $sql;
     }
 
     public function GetUserPost(){
