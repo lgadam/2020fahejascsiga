06ba7eb80bdb1dd0628ef5c2887705165b4c0f44
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-04-23 16:56:48 +0200

    Update email_verification.php
    Egy alap panel felhelyezése.

--- web/email_verification.php
+++ web/email_verification.php
@@ -9,3 +9,17 @@ if (!empty($_GET['code']) && isset($_GET['code'])) {
      if(!($account->Activate($_GET['code'])))  exitAlertRedirect('Sikertelen aktiválás', 'index.php');
 }
 ?>
+<!DOCTYPE html>
+<html lang="hu">
+<?php require ('./html/head.html');?>
+<body>
+<?php require('nav.php');?>
+<div class="container-fluid">
+    <div class="col-sm-4">
+        <div class="panel panel-default">
+        </div>
+    </div>
+    <hr>
+</div>
+<?php require('./html/footer.html'); ?>
+</html>
