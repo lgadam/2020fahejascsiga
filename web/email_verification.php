d81b020468e242f9ea2a8cb2411688a949ea1ccb
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-04-23 16:48:42 +0200

    Update email_verification.php
    A feltétel hozzáadása, ez által igazolható az email cím.

--- web/email_verification.php
+++ web/email_verification.php
@@ -1,1 +1,11 @@
-
+<?php
+if (!empty($_GET['code']) && isset($_GET['code'])) {
+    include_once("./config/config.php");
+    include_once ("./config/User.php");
+    include_once ("./functions.php");
+     $db = new Database();
+     $connection = $db->DB_Connect();
+     $account = new Account($connection);
+     if(!($account->Activate($_GET['code'])))  exitAlertRedirect('Sikertelen aktiválás', 'index.php');
+}
+?>
