cca278191ff6065dc961f3a7dd9007924d6bdbbe
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-04-25 12:12:22 +0200

    Update update.php
    Hozzáférés ellenőrzése az adatbázisból, hogy szerepel-e benne.

--- web/update.php
+++ web/update.php
@@ -1,1 +1,18 @@
+<?php
+session_start();
+if (isset($_SESSION['login'])) {
+    include_once("./config/config.php");
+    include_once("./config/User.php");
+    include_once("./functions.php");
+    $db = new Database();
+    $connection = $db->DB_Connect();
+    $update = new Account($connection);
+    $cim = $_POST['cim'];
+    $marka = $_POST['marka'];
+    $tipus = $_POST['tipus'];
+    $evjarat = $_POST['evjarat'];
+    $kmallas = $_POST['kmallas'];
+    $uzemanyag = $_POST['uzemanyag'];
+    $ar = $_POST['ar'];
 
+}
