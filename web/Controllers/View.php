78c5275ded8ff09553c4fea7856a12f1f148dab8
99balint1 <juhaszbalint99@gmail.com>
2020-04-29 11:48:37 +0200

    View.php - table class létrehozása, konstruktor megírása

---
+++ web/Controllers/View.php
@@ -0,0 +1,18 @@
+<?php
+class Table
+{
+    private $pageIndex = 1;
+    private $numOfItems;
+    private $count;
+    private $data;
+    private $pages;
+    public function __construct($data, $numOfItems, $pageIndex)
+    {
+        $this->data = $data;
+        $this->numOfItems = $numOfItems;
+        $this->pageIndex = $pageIndex;
+        $this->count = sizeof($data);
+        $this->pages = intval(ceil($this->count/$this->numOfItems));
+    }
+}
+?>
